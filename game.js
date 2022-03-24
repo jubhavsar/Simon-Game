09ac9816d891e051f47520d618a7a1deb0b2d72d
jubhavsar <juhi_bhavsar@yahoo.com>
2022-03-24 10:37:06 -0400

    Use jQuery to animate flash button and play sound

--- game.js
+++ game.js
@@ -8,9 +8,19 @@ function getRandomNumberBetween(min,max){
 }
 
 function nextSequence() {
+  
   randomNumber = getRandomNumberBetween(0,3);
   randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  console.log(randomNumber, randomChosenColour,gamePattern);
+
+  //Use jQuery to select the button with the same id as the randomChosenColour.
+  //Use jQuery to animate a flash to the button
+  $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+  //To play the sound for the button colour selected. 
+  let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+   audio.play();
+  
+ 
 }
 nextSequence();

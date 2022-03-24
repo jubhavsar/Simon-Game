2b58885327e3bc9f50519fa8a2e0171f200ecaa5
jubhavsar <juhi_bhavsar@yahoo.com>
2022-03-24 09:33:33 -0400

    Generated random number and pushed chosen color into an array

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-alert('Welcome simon');
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"]
+let randomNumber;
+let randomChosenColour;
+// generate random number
+function getRandomNumberBetween(min,max){
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function nextSequence() {
+  randomNumber = getRandomNumberBetween(0,3);
+  randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  console.log(randomNumber, randomChosenColour,gamePattern);
+}
+nextSequence();
